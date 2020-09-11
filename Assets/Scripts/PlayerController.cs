c9c0fc02f5dfb304eaf57f0d1a6a7e33a5686f5a
neoJINXD <neojinxd@gmail.com>
2020-09-11 19:59:22 -0400

    Base controls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+
+        
+    public float jumpForce = 0;
+    public int jump = 0;
+    public int jumpAmount = 1;
+
+    private Rigidbody2D rb;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        if (jump < jumpAmount && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            jump++;
+            print("yes");
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        float movement = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(movement * speed * Time.deltaTime, rb.velocity.y);
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        jump = 0;
+
+    }
+}
