7586524316575e84a689ff0f558e681684876de9
neoJINXD <neojinxd@gmail.com>
2020-09-11 21:40:04 -0400

    Following camera on player

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform player;
+    public float speed = 0.125f;
+    public Vector3 offset;
+    
+    void FixedUpdate() 
+    {
+        Vector3 newPos = player.position + offset;
+        Vector3 finalPos = Vector3.Lerp(transform.position, newPos, speed);
+        transform.position = finalPos;
+    }
+
+}
