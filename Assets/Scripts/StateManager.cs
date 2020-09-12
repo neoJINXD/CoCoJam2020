d389ff8fa627883d4a15b05fa80f33348b600705
PBMocha <joshuabrandonpb@gmail.com>
2020-09-11 21:01:23 -0400

    added, not finished, StateManager

---
+++ Assets/Scripts/StateManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager : MonoBehaviour
+{
+
+    private static StateManager instance;
+
+    //Game properties
+
+    private int maxHealth;
+    private int health;
+    private int weapon; // current weapon
+    private Dictionary<string, bool> abilities; // boolean flags for current available abilities player is allowed
+    private int level;
+    
+    void Awake() 
+    {
+        if (StateManager.instance != null && instance != this)
+        {   
+            Destroy(this.gameObject);
+        } else {
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Dictionary<string, bool> GetAbilities()
+    {
+        return abilities;
+    }
+
+    public bool PlayerCan(string ability)
+    {
+        return this.abilities[ability];
+    }
+
+    public void handleDeathReset()
+    {
+        // Reset State, convert max health to points to use for shop
+        
+        // 
+    }
+
+    
+}
